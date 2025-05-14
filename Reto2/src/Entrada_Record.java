1692d39a592d305ced749fbb3ca29ea00310e28d
Galleta4 <noemsioo33@gmail.com>
2025-05-13 22:27:10 -0600

    Delete Reto2 directory

--- Reto2/src/Entrada_Record.java
+++
@@ -1,6 +0,0 @@
-public record Entrada_Record(String nombreEvento, double precioEntrada) {
-
-    public void mostrarInformacion() {
-        System.out.println("---> Evento: " + nombreEvento + "---> Precio: $" + precioEntrada);
-    }
-}
